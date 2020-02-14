9cd72a14c324c1971e26353c2372a251508a68f8
Zach Justus <zach.justus1@gmail.com>
2020-02-13 17:15:49 -0800

    gave up on seprate data layer

--- Custom/Luminate/Door_Access/doorAccess_locations.ascx.cs
+++ Custom/Luminate/Door_Access/doorAccess_locations.ascx.cs
@@ -13,6 +13,15 @@ namespace ArenaWeb.UserControls.custom.Luminate.DoorAccess{
 	using Arena.Utility;
     using Arena.Organization; //?
 
+    using System.Collections;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using Arena.DataLib;
+    using Arena.DataLayer;
+    using Arena.DataLayer.Security;
+
+
     /// <summary>
 	///		This module manages the locations for the doors
 	/// </summary>
@@ -23,7 +32,30 @@ namespace ArenaWeb.UserControls.custom.Luminate.DoorAccess{
         #region Module Settings
         [NumericSetting("Person Page", "The person page id", false)]
         public string PersonPageSetting { get { return Setting("PersonPage", "7", false); } }
-
         #endregion
+
+        DoorAccess.locationData locationDataLayer;
+    }
+
+
+////// DATA LAYER
+    public partial class locationData : SqlData{
+        public SqlDataReader GetLocationsByParent(int parent = 0)
+        {
+            ArrayList paramList = new ArrayList();
+            if(parent != 0) paramList.Add((object) new SqlParameter("@parent_id", (object) parent));
+            try
+            {
+                return this.ExecuteSqlDataReader("cust_luminate_key_sp_get_locations", paramList);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+            }
+        }
+
     }
 }
